f794bcfca7cd703bafee195f51d91d3075965069
Jinhyun Hwang <jinhyunh0808@gmail.com>
2023-11-06 14:30:10 -0500

    Created very basic framework for me to create my apps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+//import <APP NAME> from './wherever it actually is';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        test text
       </header>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/jinapp" />} />
+            {/* <Route path="/jinapp/*" element={< />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
